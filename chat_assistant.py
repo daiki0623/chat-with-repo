1faf241c58bafe8124d444df4d13a9f077bf68ef
Daiki Murata <Daiki.Murata@ibm.com>
2024-05-21 09:11:37 +0900

    add args type

--- chat_assistant.py
+++ chat_assistant.py
@@ -10,6 +10,7 @@ from genai.schema import DecodingMethod, TextGenerationParameters
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate, SystemMessagePromptTemplate
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.messages.ai import AIMessageChunk
@@ -23,7 +24,7 @@ class ChatAssistant:
     def __init__(self) -> None:
         self._logger = my_logger.set_logger(__name__)
 
-    def respond(self, selected_model, message, is_rag):
+    def respond(self, selected_model: str, message: str, is_rag: bool):
         llm = self.__model_initializer(selected_model)
         chain = self.__create_chain(llm, is_rag)
         chain_with_message_history = RunnableWithMessageHistory(
@@ -45,7 +46,7 @@ class ChatAssistant:
             else:
                 pass
 
-    def __model_initializer(self, selected_model):
+    def __model_initializer(self, selected_model: str) -> BaseChatModel:
         """画面で選択されたモデル名からどのモデルを利用するかを判別するフラグを検索"""
         selected_model_flg = None
         for d in settings.MODEL_OPTIONS:
@@ -74,7 +75,7 @@ class ChatAssistant:
             )
         return llm
 
-    def __create_chain(self, llm, is_rag):
+    def __create_chain(self, llm: BaseChatModel, is_rag: bool):
         if is_rag:
             """VectorDBの初期化"""
             vectorstore = initialize_vectorstore()
